a568c069b551d91826ec200e1c96246544b7f9e5
Michael Werner <reznaeous@gmail.com>
2024-09-08 12:18:33 -0400

    Add RSS feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://profound-halva-e89b28.netlify.app/"
+});
