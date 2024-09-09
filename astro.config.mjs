44f901fe64b1624809b22c869b5913f57514dc30
Michael Werner <reznaeous@gmail.com>
2024-09-09 11:26:42 -0400

    Add dark mode, add hydrated island on main entry page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://profound-halva-e89b28.netlify.app/"
+  site: "https://profound-halva-e89b28.netlify.app/",
+  integrations: [preact()]
 });
